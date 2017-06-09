e2a85f8dca5856c9eb224e9e3bf7e0b97e020bb4
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-06-09 12:14:07 -0400

    set up

--- whisk/config/routes.rb
+++ whisk/config/routes.rb
@@ -1,32 +1,45 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-
-  get 'recipes/new'
-
-  get 'recipes/create'
-
-  get 'recipes/show'
-
-  get 'recipes/edit'
-
-  get 'recipes/update'
-
-  get 'recipes/destroy'
-
-  get 'desserts/index'
-
-  get 'desserts/new'
-
-  get 'desserts/create'
-
-  get 'desserts/show'
-
-  get 'desserts/edit'
-
-  get 'desserts/update'
-
-  get 'desserts/destroy'
-
   devise_for :users
+  root to: 'desserts#index'
+
+  resources :desserts do
+    resources :recipes
+  end
+
+  resources :recipes, only: [:index, :show] do
+    member do
+      post 'add_favorite'
+      delete 'remove_favorite'
+    end
+  end
+  # get 'recipes/index'
+  #
+  # get 'recipes/new'
+  #
+  # get 'recipes/create'
+  #
+  # get 'recipes/show'
+  #
+  # get 'recipes/edit'
+  #
+  # get 'recipes/update'
+  #
+  # get 'recipes/destroy'
+  #
+  # get 'desserts/index'
+  #
+  # get 'desserts/new'
+  #
+  # get 'desserts/create'
+  #
+  # get 'desserts/show'
+  #
+  # get 'desserts/edit'
+  #
+  # get 'desserts/update'
+  #
+  # get 'desserts/destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
