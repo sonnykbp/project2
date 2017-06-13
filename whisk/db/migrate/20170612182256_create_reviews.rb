34dba8472ebb0cea9b35b7089a43a0b9aa5fb76b
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-06-13 10:42:19 -0400

    updated seed

--- whisk/db/migrate/20170612182256_create_reviews.rb
+++ whisk/db/migrate/20170612182256_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
       t.text :body
+      t.date :postdate
       t.references :recipe, index: true, foreign_key: true, null: false
       t.references :user, index: true, foreign_key: true, null: false
       t.timestamps
